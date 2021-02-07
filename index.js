000ef48eaa08237c3a4ec34a9ebb70f63528d9cb
Valerio357 <62748844+Valerio357@users.noreply.github.com>
2021-02-07 22:46:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 
-var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017')
+var dbConn = mongodb.MongoClient.connect('mongodb+srv://valerio:Sharingan93@cluster0.hercr.mongodb.net/article?retryWrites=true&w=majority
+')
 
 var app = express();
 
